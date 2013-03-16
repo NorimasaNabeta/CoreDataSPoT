81a35c80335f4b0f9590f30376d58995f1b7a0e8
Norimasa Nabeta <norimasanabeta@me.com>
2013-03-16 10:45:45 +0900

    if the meaning of  the 'historical record', all of the viewing events should be insert into the database independently is correct implementation. but in this implementation does not insert the multiple records of the same photo viewing event.

--- CoreDataSPoT/Recents+Create.m
+++ CoreDataSPoT/Recents+Create.m
@@ -16,12 +16,38 @@
 {
     Recents *recent=nil;
     
+    
+    // if the meaning of 'historical records', all of the viewing event should be insert into the database indipendently.
+    // in this case, previous implementation is not wrong.
+    // if (photo) {
+    //        recent = [NSEntityDescription insertNewObjectForEntityForName:@"Recents" inManagedObjectContext:context];
+    //        recent.photos = photo;
+    //        recent.timestamp = [[NSDate date] timeIntervalSince1970];
+    // }
+    // but this implementation does not insert the multiple records for the same photo view event.
+    //
     if (photo) {
-        // NSArray *matches = [context executeFetchRequest:request error:&error];
-        recent = [NSEntityDescription insertNewObjectForEntityForName:@"Recents" inManagedObjectContext:context];
-        recent.photos = photo;
-        recent.timestamp = [[NSDate date] timeIntervalSince1970];
+        NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Recents"];
+        request.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"timestamp" ascending:YES]];
+        request.predicate = [NSPredicate predicateWithFormat:@"photos.unique = %@", photo.unique];
+        
+        NSError *error;
+        NSArray *matches = [context executeFetchRequest:request error:&error];
+        
+        if (!matches || ([matches count] > 1)) {
+            // handle error
+        } else if (![matches count]) {
+            recent = [NSEntityDescription insertNewObjectForEntityForName:@"Recents" inManagedObjectContext:context];
+            recent.photos = photo;
+            recent.timestamp = [[NSDate date] timeIntervalSince1970];
+        } else {
+            recent = [matches lastObject];
+        }
+
     }
+    
+    
+    
     
     return recent;
 }
